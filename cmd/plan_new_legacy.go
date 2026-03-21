52c8714d6b64eccb37fcc45b0199634c16b4cd3d
David Stockton <dave@davidstockton.com>
2026-03-21 09:25:36 -0600

    Make plan new deprecation more prominent

--- cmd/plan_new_legacy.go
+++ cmd/plan_new_legacy.go
@@ -13,7 +13,7 @@ var planNewLegacyCmd = &cobra.Command{
 	Short:  "Deprecated: use 'fil new plan' instead",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Fprintln(os.Stderr, "")
-		fmt.Fprintln(os.Stderr, "  *** 'fil plan new' has been removed. Use 'fil new plan' instead. ***")
+		fmt.Fprintln(os.Stderr, "  \033[31m*** 'fil plan new' has been removed. Use 'fil new plan' instead. ***\033[0m")
 		fmt.Fprintln(os.Stderr, "")
 		os.Exit(1)
 		return nil
