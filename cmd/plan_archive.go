d9f02d44724c8d9042312c4a4d98a979e0cfa65e
David Stockton <dave@davidstockton.com>
2026-04-01 21:00:09 -0600

    Add --pick flag to archive to allow interactive plan picking

--- cmd/plan_archive.go
+++ cmd/plan_archive.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/dstockto/fil/api"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -28,6 +29,8 @@ var planArchiveCmd = &cobra.Command{
 			}
 		}
 
+		pick, _ := cmd.Flags().GetBool("pick")
+
 		var discovered []DiscoveredPlan
 		if len(args) > 0 {
 			discovered = append(discovered, DiscoveredPlan{Path: args[0], DisplayName: FormatPlanPath(args[0])})
@@ -36,10 +39,10 @@ var planArchiveCmd = &cobra.Command{
 			discovered = plans
 		}
 
+		// Build list of completed plans
+		var completed []DiscoveredPlan
 		for _, dp := range discovered {
 			plan := dp.Plan
-			// If from args and not yet loaded, the Plan will be zero-value.
-			// We need to load it for args case.
 			if len(args) > 0 && !dp.Remote {
 				data, err := os.ReadFile(dp.Path)
 				if err != nil {
@@ -48,6 +51,7 @@ var planArchiveCmd = &cobra.Command{
 				if err := loadPlanYAML(data, &plan); err != nil {
 					continue
 				}
+				dp.Plan = plan
 			}
 
 			allDone := true
@@ -59,39 +63,93 @@ var planArchiveCmd = &cobra.Command{
 			}
 
 			if allDone {
-				if dp.Remote {
-					client := api.NewPlanServerClient(Cfg.PlansServer, version, Cfg.TLSSkipVerify)
-					if err := client.ArchivePlan(context.Background(), dp.RemoteName); err != nil {
-						fmt.Printf("  Error archiving remote plan %s: %v\n", dp.DisplayName, err)
-					} else {
-						fmt.Printf("Archived %s\n", dp.DisplayName)
-					}
-				} else {
-					if Cfg.ArchiveDir == "" {
-						fmt.Printf("Skipping %s (archive_dir not configured for local archiving)\n", dp.DisplayName)
-						continue
-					}
-					ext := filepath.Ext(dp.Path)
-					base := strings.TrimSuffix(filepath.Base(dp.Path), ext)
-					timestamp := time.Now().Format("20060102150405")
-					newFilename := fmt.Sprintf("%s-%s%s", base, timestamp, ext)
-
-					dest := filepath.Join(Cfg.ArchiveDir, newFilename)
-					fmt.Printf("Archiving %s to %s\n", dp.DisplayName, FormatPlanPath(dest))
-					err := os.Rename(dp.Path, dest)
-					if err != nil {
-						fmt.Printf("  Error moving file: %v\n", err)
-					}
-				}
+				completed = append(completed, dp)
 			} else {
 				fmt.Printf("Skipping %s (not all projects are completed)\n", dp.DisplayName)
 			}
+		}
+
+		if len(completed) == 0 {
+			fmt.Println("No completed plans to archive.")
+			return nil
+		}
+
+		// If --pick, let the user select which plan to archive
+		if pick && len(args) == 0 {
+			selected, err := selectPlanToArchive(completed)
+			if err != nil {
+				return err
+			}
+			if selected == nil {
+				fmt.Println("No plan selected.")
+				return nil
+			}
+			completed = []DiscoveredPlan{*selected}
+		}
+
+		for _, dp := range completed {
+			archivePlan(dp)
 		}
 
 		return nil
 	},
 }
 
+func archivePlan(dp DiscoveredPlan) {
+	if dp.Remote {
+		client := api.NewPlanServerClient(Cfg.PlansServer, version, Cfg.TLSSkipVerify)
+		if err := client.ArchivePlan(context.Background(), dp.RemoteName); err != nil {
+			fmt.Printf("  Error archiving remote plan %s: %v\n", dp.DisplayName, err)
+		} else {
+			fmt.Printf("Archived %s\n", dp.DisplayName)
+		}
+	} else {
+		if Cfg.ArchiveDir == "" {
+			fmt.Printf("Skipping %s (archive_dir not configured for local archiving)\n", dp.DisplayName)
+			return
+		}
+		ext := filepath.Ext(dp.Path)
+		base := strings.TrimSuffix(filepath.Base(dp.Path), ext)
+		timestamp := time.Now().Format("20060102150405")
+		newFilename := fmt.Sprintf("%s-%s%s", base, timestamp, ext)
+
+		dest := filepath.Join(Cfg.ArchiveDir, newFilename)
+		fmt.Printf("Archiving %s to %s\n", dp.DisplayName, FormatPlanPath(dest))
+		err := os.Rename(dp.Path, dest)
+		if err != nil {
+			fmt.Printf("  Error moving file: %v\n", err)
+		}
+	}
+}
+
+func selectPlanToArchive(plans []DiscoveredPlan) (*DiscoveredPlan, error) {
+	if len(plans) == 1 {
+		return &plans[0], nil
+	}
+
+	items := make([]string, len(plans))
+	for i, dp := range plans {
+		items[i] = dp.DisplayName
+	}
+
+	prompt := promptui.Select{
+		Label: "Select a plan to archive",
+		Items: items,
+		Size:  10,
+	}
+
+	idx, _, err := prompt.Run()
+	if err != nil {
+		if err == promptui.ErrInterrupt || err == promptui.ErrEOF {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &plans[idx], nil
+}
+
 func init() {
+	planArchiveCmd.Flags().BoolP("pick", "p", false, "interactively select which completed plan to archive")
 	planCmd.AddCommand(planArchiveCmd)
 }
